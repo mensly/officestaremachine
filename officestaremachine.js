51ee02fed18e4b4d1eda11e791650039819ada41
Michael Ensly <michael.ensly@gmail.com>
2017-01-06 00:49:55 +1100

    Removed unneeded stuff

--- officestaremachine.js
+++ officestaremachine.js
@@ -1,20 +1,8 @@
 // by Joe Sabia and Aaron Rasmussen
 // feel free to use this for your own purposes!
 
-var videoClips=new Array();
-
-var emotionNameToKeyEmotionName = {};
 var emotionToKey = {};
 var keyToVideo = {};
-var playHeads = {};
-var NUMVIDEOS = 720;
-var numberOfVideosPlayed = 0;
-var currentKey = 'sad';
-var currentPlays = 0;
-var allowAutoPlay = false;
-
-function isMobile() { var check = false; (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|ipad|iphone|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera); return check; }
-
 
 emotionToKey["1999"] = "1999";
 emotionToKey["amused"] = "amused";
@@ -527,8 +515,6 @@ emotionToKey["upset"] = "worried";
 emotionToKey["worried"] = "worried";
 
 
-playHeads['1999'] = 0;
-
 keyToVideo['1999'] = new Array ("TA8oR4PUdZU",
 "zgKY8adiM10",
 "dKMj48VjgJM",
@@ -558,8 +544,6 @@ keyToVideo['1999'] = new Array ("TA8oR4PUdZU",
 "905_6v6a4GY",
 "L9CBq4Da7P4");
 
-playHeads['amused'] = 0;
-
 keyToVideo['amused'] = new Array ("thx77DeHLZY",
 "aXHAos9M7_k",
 "G9uwTUxh-0w",
@@ -579,8 +563,6 @@ keyToVideo['amused'] = new Array ("thx77DeHLZY",
 "CXxlWRBHOZw",
 "zPp-QvYtqGk",
 "Sm5T2H4kEWU");
-
-playHeads['angry'] = 0;
 
 keyToVideo['angry'] = new Array ("93F60yL5kMw",
 "o6eiebBdiD4",
@@ -633,8 +615,6 @@ keyToVideo['angry'] = new Array ("93F60yL5kMw",
 "8MznS0dz3uo",
 "LL4QnbfH4aY",
 "OB36BEkbGzs");
-
-playHeads['annoyed'] = 0;
 
 keyToVideo['annoyed'] = new Array("xp12AxZstak",
 "gwo3ksZ6F24",
@@ -690,8 +670,6 @@ keyToVideo['annoyed'] = new Array("xp12AxZstak",
 "qtngXXRrJqs",
 "09nfnsUlnH4");
 
-playHeads['bored'] = 0;
-
 keyToVideo['bored'] = new Array("KO45TUVjayA",
 "ndmdheKX5h4",
 "EZ6VwS7Z7nc",
@@ -706,12 +684,8 @@ keyToVideo['bored'] = new Array("KO45TUVjayA",
 "_e2AJ95VLh8",
 "Vc-rc9QtQWI");
 
-playHeads['gruntled'] = 0;
-
 keyToVideo['gruntled'] = new Array("2joetGgluqc",
 "2joetGgluqc");
-
-playHeads['confident'] = 0;
 
 keyToVideo['confident'] = new Array("zi_L9LE3l-U",
 "giSCMyP7v7Q",
@@ -728,8 +702,6 @@ keyToVideo['confident'] = new Array("zi_L9LE3l-U",
 "NSwn6eD5qjQ",
 "CQYTl8EEgC4",
 "E-m3Y_hpl1M");
-
-playHeads['confused'] = 0;
 
 keyToVideo['confused'] = new Array("vmnoUN2_3w8",
 "1cm5CGS3W0M",
@@ -790,22 +762,16 @@ keyToVideo['confused'] = new Array("vmnoUN2_3w8",
 "7rKHBAbsuYg",
 "tY92_yGq_w8");
 
-playHeads['crazy'] = 0;
-
 keyToVideo['crazy'] = new Array("Z_TSkPivPpg",
 "NuHVr6GLhWo",
 "_dw4kgWLV1I",
 "3rvxCiazeHw");
-
-playHeads['creepy'] = 0;
 
 keyToVideo['creepy'] = new Array("5mMRrkMmXjI",
 "wSslbmoRpK4",
 "h1ZEgnuGH2Q",
 "5Ae8-Ehm1pU",
 "kk1LcL7dZTc");
-
-playHeads['defeated'] = 0;
 
 keyToVideo['defeated'] = new Array("0roSsayezn4",
 "MFjIMLP18jI",
@@ -825,8 +791,6 @@ keyToVideo['defeated'] = new Array("0roSsayezn4",
 "mCcqHb5f264",
 "oYzf4j_sjCs",
 "HkYlg8kob_w");
-
-playHeads['devious'] = 0;
 
 keyToVideo['devious'] = new Array(
 "zTIsDB35QQw",
@@ -848,8 +812,6 @@ keyToVideo['devious'] = new Array(
 "xzHXnS8B4Mo",
 "f4tGfaaV8Xw");
 
-playHeads['disgusted'] = 0;
-
 keyToVideo['disgusted'] = new Array("BV4h9JPbG2o",
 "IfRCjXzBmhQ",
 "WuFnfpYLD98",
@@ -867,13 +829,9 @@ keyToVideo['disgusted'] = new Array("BV4h9JPbG2o",
 "odXVji3r664",
 "SwTxkGPc2bo");
 
-playHeads['drunk'] = 0;
-
 keyToVideo['drunk'] = new Array("I2EM9aucz6s",
 "oWgDT9J-k5c",
 "4XR-3kwaNKg");
-
-playHeads['embarrassed'] = 0;
 
 keyToVideo['embarrassed'] = new Array("rPvBS1wR89I",
 "5-7e3hYTvO4",
@@ -911,13 +869,9 @@ keyToVideo['embarrassed'] = new Array("rPvBS1wR89I",
 "0R1lFjsjZoI",
 "HdlzU6d1EJw");
 
-playHeads['evil'] = 0;
-
 keyToVideo['evil'] = new Array("rPTABGn52-Q",
 "rPTABGn52-Q",
 "kYy36UbZ92o");
-
-playHeads['funny'] = 0;
 
 keyToVideo['funny'] = new Array("TJxwipvr9mc",
 "gk2zutgRKkw",
@@ -932,12 +886,8 @@ keyToVideo['funny'] = new Array("TJxwipvr9mc",
 "GiJ3-Nwr59M",
 "WsXC3gBPcag");
 
-playHeads['guilty'] = 0;
-
 keyToVideo['guilty'] = new Array("U2TvBfzEhVo",
 "nJk8RcWG5_M");
-
-playHeads['happy'] = 0;
 
 keyToVideo['happy'] = new Array("w0XZDn94-Js",
 "LizbGz29sRA",
@@ -1021,8 +971,6 @@ keyToVideo['happy'] = new Array("w0XZDn94-Js",
 "AqI3WgMI2Dk",
 "1mf7DW77tno");
 
-playHeads['horny'] = 0;
-
 keyToVideo['horny'] = new Array("LbKdoJqJWy8",
 "9zwun7ylOF8",
 "gh1yk8kS8gI",
@@ -1035,15 +983,11 @@ keyToVideo['horny'] = new Array("LbKdoJqJWy8",
 "D4Vj-78YlTQ",
 "UaQR__YcAEg");
 
-playHeads['hungry'] = 0;
-
 keyToVideo['hungry'] = new Array("L08kScTCJ8E",
 "emZG_EchST4",
 "GRKPlbJXL7c",
 "5qNdPYpdeis",
 "UgtjZOuiP6g");
-
-playHeads['intrigued'] = 0;
 
 keyToVideo['intrigued'] = new Array("wRmIxCBJHyo",
 "PLwKLVHpE4I",
@@ -1058,8 +1002,6 @@ keyToVideo['intrigued'] = new Array("wRmIxCBJHyo",
 "nXWpP9CqfB0",
 "vuJgT7QQ-7w");
 
-playHeads['jealous'] = 0;
-
 keyToVideo['jealous'] = new Array("5XrQkhFHBfE",
 "SkvxeTVAOO0",
 "zIutEXJ5j_g");
@@ -1067,12 +1009,8 @@ keyToVideo['jealous'] = new Array("5XrQkhFHBfE",
 
 
 
-playHeads['pensive'] = 0;
-
 keyToVideo['pensive'] = new Array("mEGHM4UdBqM",
 "usD2gZYuDc0");
-
-playHeads['proud'] = 0;
 
 keyToVideo['proud'] = new Array("ZEOThui7lRE",
 "sj5LxLUcNRo",
@@ -1095,8 +1033,6 @@ keyToVideo['proud'] = new Array("ZEOThui7lRE",
 "NIaQKOo7hUs",
 "t794Kjr9H2M");
 
-playHeads['relieved'] = 0;
-
 keyToVideo['relieved'] = new Array("wnLuUsFPCP4",
 "Mxb87asgL7U",
 "AZNdGkdoYz4",
@@ -1109,8 +1045,6 @@ keyToVideo['relieved'] = new Array("wnLuUsFPCP4",
 "inB5BVMT_0E",
 "o6f2K_3afOQ",
 "fxsstAjTg4U");
-
-playHeads['sad'] = 0;
 
 keyToVideo['sad'] = new Array("eQSUrp-Ybu8",
 "YLq5z-L-kcY",
@@ -1149,8 +1083,6 @@ keyToVideo['sad'] = new Array("eQSUrp-Ybu8",
 "p_Z6ybOaVeE",
 "oiaE0CFMkSs",
 "zemlhQe_oY0");
-
-playHeads['serious'] = 0;
 
 keyToVideo['serious'] = new Array ("wbobYVA2NEE",
 "WNiwGblaQrE",
@@ -1199,13 +1131,9 @@ keyToVideo['serious'] = new Array ("wbobYVA2NEE",
 "iTW3NS20ppY",
 "qeBbsAqGlqc");
 
-playHeads['stupid'] = 0;
-
 keyToVideo['stupid'] = new Array("qR-JyIXevXw",
 "g3_ZvhXtOsM",
 "HXG5-XTih0Y");
-
-playHeads['surprised'] = 0;
 
 keyToVideo['surprised'] = new Array("HlEb6hNSaBg",
 "S4hxiKIunCk",
@@ -1256,8 +1184,6 @@ keyToVideo['surprised'] = new Array("HlEb6hNSaBg",
 "5yVKokkiskI",
 "ax8zG3wapOs",
 "HJ03Dory9vI");
-
-playHeads['worried'] = 0;
 
 keyToVideo['worried'] = new Array("Tk8h31sTxws",
 "ON90FzdtPP0",
@@ -1336,645 +1262,3 @@ keyToVideo['worried'] = new Array("Tk8h31sTxws",
 "bTW5JzxXaYs",
 "QgNuVYrNs78",
 "RgUzbro-CdY");
-
-
-var textDirtyFlag = false; //determines if the user has entered data that has not been submitted
-
-
-	function emote(){
-		$("#player").slideDown("slow");
-		$("#stareCount").slideDown("slow");
-		$("#staticimage").slideUp("slow");
-		  var videoSelector = 0;
-	  var value = $("#videoUrl").val().toLowerCase();
-
-	  if ( keyToVideo[emotionToKey[value]] ){
-
-	_gaq.push(['_trackEvent', 'Videos', 'Play', value]);
-	  		currentPlays = 0;
-	  		allowAutoPlay = true;
-	  		if ( playHeads[emotionToKey[value]] < keyToVideo[emotionToKey[value]].length -1 ){
-		  		numberOfVideosPlayed++;
-	  		if (numberOfVideosPlayed >= 706){
-		  			player.loadVideoById("fkqVDWjq3XI");
-		  		}
-		  		currentKey = emotionToKey[value];
-				player.loadVideoById(keyToVideo[emotionToKey[value]][playHeads[emotionToKey[value]]]);	
-	  			playHeads[emotionToKey[value]]++;
-        		currentPlays++;
-	  			$("#numberOfViews").text(numberOfVideosPlayed.toString());
-	
-	  		}
-	  		else {
-	  			playHeads[emotionToKey[value]] = 0;
-
-	  		}
-		}
-		else{
-
-			_gaq.push(['_trackEvent', 'Videos', 'MissingWord', value]);
-			alert("Oops. Something's up. Are you sure this is an emotion? If it is, we'll fix it. But TRY AGAIN!");
-		}
-	}
-
-
-$(function(){
-  $(".button").click(function () {
-		$( "input#videoUrl").autocomplete( "close" );
-		emote();
-		textDirtyFlag = false;
-      });
-  $(".button").on("tap",function(){
-		emote();
-		textDirtyFlag = false;
-	});
-});
-
-$(document).ready(function(){
-  if ( isMobile() ){
-       $("#staticimage").html("<img src = \"instructionimage.png\">");
-       }
-
-  $("input").keypress(function(e){
-	   textDirtyFlag = true;
-	   if (e.which == 13) {
-		$( "input#videoUrl").autocomplete( "close" );
-  		emote();
-		    textDirtyFlag = false;
-		  	event.preventDefault()
-	   }
-  });
- $("input").click(function(){
-	   $('#videoUrl').css("color", "#000");
-	if ( $('#videoUrl').val() == "e.g. happy or sad"){
-		 $('#videoUrl').val("") ;
-	}
-  });
-
-
-  $('#videoUrl').effect( "highlight", "slow").effect("highlight", "slow").effect("highlight", "slow").effect("highlight", "slow").effect("highlight", "slow").effect("highlight", "slow").effect("highlight", "slow").effect("highlight", "slow");
-  
-});
-
-  $(function() {
-    var availableTags = ["charmed",
-"cheery",
-"1999",
-"diverted",
-"amusing",
-"jealous",
-"horny",
-"sexy",
-"flirty",
-"stupid",
-"dumb",
-"sexual",
-"awkward",
-"employed",
-"funny",
-"goofy",
-"sensitive",
-"silly",
-"frustrated",
-"frustration",
-"excited",
-"deep",
-"engaged",
-"engrossed",
-"entertained",
-"tickled",
-"acrimonious",
-"apoplectic",
-"bad-tempered",
-"bitter",
-"blank",
-"drunk",
-"infuriated",
-"choleric",
-"cross",
-"enraged",
-"fuming",
-"furious",
-"heated",
-"ill-natured",
-"ill-tempered",
-"incensed",
-"indignant",
-"infurious",
-"lonely",
-"aroused",
-"grumpy",
-"irate",
-"livid",
-"mad",
-"outraged",
-"passionate",
-"pissed",
-"aggressive",
-"raging",
-"seething",
-"sore",
-"steamed",
-"stormy",
-"vexed",
-"wrathful",
-"angry",
-"aggravated",
-"antagonized",
-"attacked",
-"bugged",
-"chagrined",
-"disgruntled",
-"displeased",
-"exasperated",
-"frosty",
-"galled",
-"irked",
-"irritated",
-"intense",
-"gruntled",
-"miffed",
-"nerved",
-"nettled",
-"peeved",
-"piqued",
-"riled",
-"ticked",
-"unnerved",
-"annoyed",
-"disinterested",
-"dull",
-"stultified",
-"stupefied",
-"ennui",
-"tedious",
-"uninterested",
-"complacent",
-"bored",
-"arrogant",
-"assertive",
-"assured",
-"certain",
-"committed",
-"convinced",
-"coolheaded",
-"determined",
-"hopeful",
-"imperturbable",
-"levelheaded",
-"optimistic",
-"persuaded",
-"poised",
-"positive",
-"resolute",
-"sanguine",
-"self-assured",
-"confident",
-"self-confident",
-"self-possessed",
-"self-reliant",
-"unperturbed",
-"unruffled",
-"hilarious",
-"addled",
-"befuddled",
-"bewildered",
-"blurred",
-"confounded",
-"demented",
-"dim",
-"disorientated",
-"disoriented",
-"hazy",
-"imprecise",
-"indistinct",
-"muddled",
-"mystified",
-"nonplussed",
-"senile",
-"shadowy",
-"sketchy",
-"unbalanced",
-"unclear",
-"unhinged",
-"vague",
-"perplexed",
-"perplexing",
-"woolly",
-"confused",
-"acquiescent",
-"compliant",
-"dismissed",
-"fatalistic",
-"forbearing",
-"passive",
-"patient",
-"philosophical",
-"rejected",
-"resigned",
-"submissive",
-"uncomplaining",
-"defeated",
-"underhanded",
-"deceitful",
-"dishonest",
-"dishonorable",
-"regret",
-"regretful",
-"love",
-"loving",
-"fear",
-"fearful",
-"smug",
-"sarcastic",
-"contempt",
-"embarassed",
-"unethical",
-"unprincipled",
-"immoral",
-"unscrupulous",
-"evil",
-"devilish",
-"diabolical",
-"hungry",
-"starving",
-"demonic",
-"satanic",
-"sultry",
-"romantic",
-"lovey",
-"fraudulent",
-"dubious",
-"unfair",
-"treacherous",
-"duplicitous",
-"crafty",
-"cunning",
-"calculating",
-"artful",
-"conniving",
-"scheming",
-"sly",
-"lethargic",
-"sardonic",
-"wily",
-"sneaky",
-"furtive",
-"secretive",
-"clandestine",
-"surreptitious",
-"covert",
-"snide",
-"crooked",
-"shady",
-"dirty",
-"devious",
-"appalled",
-"lazy",
-"jaded",
-"sleepy",
-"horrified",
-"nauseating",
-"offensive",
-"offended",
-"repellent",
-"repulsive",
-"scandalized",
-"shocking",
-"icy",
-"sick",
-"sickening",
-"disgusted",
-"abashed",
-"ashamed",
-"shameful",
-"discomposed",
-"bashful",
-"blushing",
-"discomfited",
-"disconcerted",
-"flustered",
-"humiliated",
-"mortified",
-"self-conscious",
-"shamed",
-"shamefaced",
-"sheepish",
-"shy",
-"tongue-tied",
-"uncomfortable",
-"embarrassed",
-"ebullient",
-"ridiculous",
-"beaming",
-"beatific",
-"extreme",
-"blissful",
-"blithe",
-"mean",
-"crying",
-"sassy",
-"weird",
-"creepy",
-"laughing",
-"excitable",
-"laughter",
-"crazy",
-"drunk",
-"insane",
-"scary",
-"deadly",
-"homicidal",
-"suprised",
-"murderous",
-"dangerous",
-"anger",
-"cranky",
-"buoyant",
-"carefree",
-"cheerful",
-"sappy",
-"chirpy",
-"sentimental",
-"sappy",
-"bummed",
-"contented",
-"delighted",
-"ecstatic",
-"elated",
-"euphoric",
-"exhilarated",
-"exultant",
-"gleeful",
-"gratified",
-"grinning",
-"jocular",
-"jocund",
-"jolly",
-"jovial",
-"joyful",
-"joyous",
-"jubilant",
-"lighthearted",
-"merry",
-"overjoyed",
-"hangry",
-"pleased",
-"radiant",
-"rapturous",
-"satisfied",
-"smiling",
-"sunny",
-"thrilled",
-"untroubled",
-"guilty",
-"happy",
-"absorbing",
-"appealing",
-"arousing",
-"captivated",
-"compelling",
-"curious",
-"enchanted",
-"enthralled",
-"fascinated",
-"gripping",
-"interested",
-"piquing",
-"riveting",
-"intrigued",
-"content",
-"glad",
-"envious",
-"grateful",
-"thankful",
-"proud",
-"reassured",
-"reassuring",
-"relieved",
-"awful",
-"blah",
-"blue",
-"crestfallen",
-"contemplative",
-"dejected",
-"seductive",
-"depressed",
-"desolate",
-"despairing",
-"despondent",
-"disconsolate",
-"dismal",
-"doleful",
-"down",
-"downcast",
-"forlorn",
-"gloomy",
-"glum",
-"harrowing",
-"heartbreaking",
-"heartbroken",
-"heartrending",
-"inconsolable",
-"melancholy",
-"miserable",
-"mournful",
-"pathetic",
-"pitiful",
-"sorrowful",
-"sorry",
-"tragic",
-"traumatic",
-"unfortunate",
-"unhappy",
-"woebegone",
-"wretched",
-"hurt",
-"sad",
-"apathetic",
-"bemused",
-"stoked",
-"callous",
-"deadpan",
-"cold",
-"cool",
-"dour",
-"earnest",
-"emotionless",
-"frigid",
-"genuine",
-"grave",
-"constipated",
-"grim",
-"humorless",
-"impassive",
-"passionless",
-"pensive",
-"quixotic",
-"perfunctory",
-"phlegmatic",
-"poker-faced",
-"reserved",
-"restrained",
-"sincere",
-"sober",
-"solemn",
-"somber",
-"stern",
-"stoic",
-"stoical",
-"stony-faced",
-"thoughtful",
-"undemonstrative",
-"unemotional",
-"unexcitable",
-"unfeeling",
-"unsentimental",
-"unsmiling",
-"wholehearted",
-"serious",
-"amazed",
-"astonished",
-"indifferent",
-"astounded",
-"dazed",
-"flabbergasted",
-"dumbfounded",
-"dumbstruck",
-"disbelieving",
-"floored",
-"flummoxed",
-"shaken",
-"shocked",
-"speechless",
-"staggered",
-"startled",
-"stunned",
-"thunderstruck",
-"incredulous",
-"surprised",
-"bushed",
-"dead",
-"dog-tired",
-"drained",
-"enervated",
-"exhausted",
-"fatigued",
-"knackered",
-"pooped",
-"wasted",
-"weary",
-"zonked",
-"tired",
-"agitated",
-"agonized",
-"anxious",
-"bedeviled",
-"bothered",
-"brooding",
-"concerned",
-"disquieted",
-"distressed",
-"disturbed",
-"fretted",
-"nervous",
-"panicked",
-"perturbed",
-"afraid",
-"plagued",
-"terrified",
-"preoccupied",
-"rattled",
-"scared",
-"stewed",
-"stressed",
-"taxing",
-"tormented",
-"troubled",
-"unsettled",
-"upset",
-"worried"
-    ];
-    $( "input#videoUrl" ).autocomplete({
-      source: availableTags
-    });
-  });
-
-
-		
-	
-
-    // create youtube player
-    var player;
-    function onYouTubePlayerAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'QKCFDhMooQA',
-     playerVars: {rel: 0},
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-
-
-    }
-
-    // autoplay video
-    function onPlayerReady(event) {
-	  if ( !isMobile() ){
-		$("#player").slideDown("slow");
-		$("#staticimage").slideUp("slow");}
-		player.loadVideoById("QKCFDhMooQA");
-	    }
-
-    // when video ends
-    function onPlayerStateChange(event) {      
-   
-        if(event.data === 0) {       // end of video detected
-        	if (!allowAutoPlay){
-        		return;
-        	}
-
-        	if ( playHeads[emotionToKey[currentKey]] < keyToVideo[emotionToKey[currentKey]].length ){
-				_gaq.push(['_trackEvent', 'Videos', 'AutoPlay', currentKey]);			
-				player.loadVideoById(keyToVideo[emotionToKey[currentKey]][playHeads[emotionToKey[currentKey]]]);
-	  			playHeads[emotionToKey[currentKey]]++;
-		  		numberOfVideosPlayed++;
-	  		if (numberOfVideosPlayed >= 706){
-		  			player.loadVideoById("fkqVDWjq3XI");
-		  		}
-
-		  		currentPlays++;
-
-	  			$("#numberOfViews").text(numberOfVideosPlayed.toString());
-	  		}
-	  		else {
-	  			currentPlays = 0;
-        		player.loadVideoById("fVbyBoW1HIM");
-        		allowAutoPlay = false;
-	  			playHeads[emotionToKey[currentKey]] = 0;
-	  			return;
-	  		}
-	
-        }
-        
-    }
-	
-	function loadVideo(value){
-			
-			player.loadVideoById(videoClips[value]);	
-			current_video = value;
-	}
-	
-function playWordTag(theWord){
-
-	  var value = $("#videoUrl").val(theWord);
-	  emote();
-
-}
-
-
-
-
